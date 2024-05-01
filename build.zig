e1a9698723c8d8924133e75a016e5350af64a539
David P. Sugar <david@thesugar.de>
2024-05-01 18:30:00 +0200

    moved from Zig 0.11.0 to 0.12.0

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     const uuid_module = b.addModule("uuid", .{
-        .source_file = .{ .path = "src/main.zig" },
+        .root_source_file = .{ .path = "src/main.zig" },
     });
 
     try b.modules.put(b.dupe("uuid"), uuid_module);
@@ -27,15 +27,15 @@ pub fn build(b: *std.Build) !void {
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
 
-    const v4_example = addExample(b, uuid_module, "v4-example", "examples/uuid_v4.zig");
-    const v7_example = addExample(b, uuid_module, "v7-example", "examples/uuid_v7.zig");
+    const v4_example = addExample(b, uuid_module, "v4-example", "examples/uuid_v4.zig", target);
+    const v7_example = addExample(b, uuid_module, "v7-example", "examples/uuid_v7.zig", target);
 
     const run_v4_example = b.step("run-v4-example", "Run the v4 example");
     run_v4_example.dependOn(&v4_example.step);
     const run_v7_example = b.step("run-v7-example", "Run the v7 example");
     run_v7_example.dependOn(&v7_example.step);
 
-    const run_bench = addBenchmark(b, uuid_module, "bench", "bench/main.zig");
+    const run_bench = addBenchmark(b, uuid_module, "bench", "bench/main.zig", target);
     if (b.args) |args| {
         run_bench.addArgs(args);
     }
@@ -43,24 +43,26 @@ pub fn build(b: *std.Build) !void {
     bench.dependOn(&run_bench.step);
 }
 
-fn addExample(b: *std.build.Builder, uuid_module: *std.build.Module, exeName: []const u8, sourceFile: []const u8) *std.build.RunStep {
+fn addExample(b: *std.Build, uuid_module: *std.Build.Module, exeName: []const u8, sourceFile: []const u8, target: std.Build.ResolvedTarget) *std.Build.Step.Run {
     const exe = b.addExecutable(.{
         .name = exeName,
         .root_source_file = .{ .path = sourceFile },
+        .target = target,
     });
-    exe.addModule("uuid-zig", uuid_module);
+    exe.root_module.addImport("uuid-zig", uuid_module);
     b.installArtifact(exe);
 
     return b.addRunArtifact(exe);
 }
 
-fn addBenchmark(b: *std.build.Builder, uuid_module: *std.build.Module, exeName: []const u8, sourceFile: []const u8) *std.build.RunStep {
+fn addBenchmark(b: *std.Build, uuid_module: *std.Build.Module, exeName: []const u8, sourceFile: []const u8, target: std.Build.ResolvedTarget) *std.Build.Step.Run {
     const exe = b.addExecutable(.{
         .name = exeName,
         .root_source_file = .{ .path = sourceFile },
         .optimize = .ReleaseFast,
+        .target = target,
     });
-    exe.addModule("uuid-zig", uuid_module);
+    exe.root_module.addImport("uuid-zig", uuid_module);
     b.installArtifact(exe);
 
     return b.addRunArtifact(exe);
